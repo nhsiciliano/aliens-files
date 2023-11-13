091b91b19339cd8148892f4382b78539a844f8df
nhsiciliano <nhsiciliano@gmail.com>
2023-11-13 19:00:22 -0300

    api

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 import { notFound } from 'next/navigation'
 
 async function getData(id) {
-    const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+    const res = await fetch(`https://aliens-files-cijzqssuo-nhsiciliano.vercel.app/api/posts/${id}`, {
         cache: "no-store",
     });
 
