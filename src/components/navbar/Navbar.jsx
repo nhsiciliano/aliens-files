bd68aeb924d85ebd5fe473f772fc30c36d49c4ae
nhsiciliano <nhsiciliano@gmail.com>
2023-11-13 19:07:32 -0300

    dtoggle

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -71,6 +71,7 @@ const Navbar = () => {
                         {link.title}
                     </Link>
                 ))}
+                <DarkModeToggle />
                 {session.status === "authenticated" && (
                     <button 
                         className={styles.hamlogout}
